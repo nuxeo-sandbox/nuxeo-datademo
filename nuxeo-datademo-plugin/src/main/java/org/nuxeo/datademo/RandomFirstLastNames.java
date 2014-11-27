20b669ea9291c98b3559b6c3e3ce7d87ae0289cb
Thibaud Arguillere <thibarg.web@free.fr>
2014-11-27 14:43:06 -0500

    Handle random first-last names

---
+++ nuxeo-datademo-plugin/src/main/java/org/nuxeo/datademo/RandomFirstLastNames.java
@@ -0,0 +1,165 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thibaud Arguillere
+ */
+package org.nuxeo.datademo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.common.utils.FileUtils;
+
+/**
+ *
+ *
+ * @since 7.1
+ */
+public class RandomFirstLastNames {
+
+    private static Log log = LogFactory.getLog(RandomFirstLastNames.class);
+
+    public enum GENDER {
+        MALE, FEMALE, ANY
+    };
+
+    protected static ArrayList<String> firstNamesMale = null;
+
+    protected static int fnMaleMaxForRandom = -1;
+
+    protected static ArrayList<String> firstNamesFemale = null;
+
+    protected static int fnFemaleMaxForRandom = -1;
+
+    protected static ArrayList<String> lastNames = null;
+
+    protected static int lnMaxForRandom = -1;
+
+    private static RandomFirstLastNames instance = null;
+
+    private static int usageCount = 0;
+
+    protected ArrayList<String> loadFile(String inLocalPath) throws IOException {
+
+        ArrayList<String> as = new ArrayList<String>();
+
+        File f = FileUtils.getResourceFileFromContext(inLocalPath);
+        try (BufferedReader reader = Files.newBufferedReader(f.toPath())) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if(!line.isEmpty()) {
+                    as.add(line);
+                }
+            }
+        }
+
+        return as;
+    }
+
+    protected RandomFirstLastNames() throws IOException {
+        firstNamesMale = loadFile("files/FirstNames-Male.txt");
+        fnMaleMaxForRandom = firstNamesMale.size() - 1;
+
+        firstNamesFemale = loadFile("files/FirstNames-Female.txt");
+        fnFemaleMaxForRandom = firstNamesFemale.size() - 1;
+
+        lastNames = loadFile("files/LastNames.txt");
+        lnMaxForRandom = lastNames.size() - 1;
+    }
+
+    protected void cleanup() {
+        firstNamesMale = null;
+        firstNamesFemale = null;
+        lastNames = null;
+
+        fnMaleMaxForRandom = -1;
+        fnFemaleMaxForRandom = -1;
+        lnMaxForRandom = -1;
+    }
+
+    /**
+     * Load in memory the files used to get random values.
+     *
+     * It is recommenced to explicitly call <code>release()</code> once you are
+     * done with getting values
+     *
+     * @return
+     * @throws IOException
+     *
+     * @since 7.1
+     */
+    public synchronized static RandomFirstLastNames getInstance()
+            throws IOException {
+
+        if (instance == null) {
+            instance = new RandomFirstLastNames();
+        }
+
+        usageCount += 1;
+        return instance;
+    }
+
+    /**
+     * It is recommended to explicitly release the object once you don't need it
+     * anymore, to avoid taking room in memory while not used at all
+     *
+     *
+     * @since 7.1
+     */
+    public synchronized static void release() {
+
+        usageCount -= 1;
+        if(usageCount == 0) {
+            instance.cleanup();
+            instance = null;
+        }
+
+        if(usageCount < 0) {
+            usageCount = 0;
+            log.error("Releasing the instance too many time");
+        }
+    }
+
+    public synchronized static int getUsageCount() {
+        return usageCount;
+    }
+
+    public String getFirstName(GENDER inKind) {
+
+        switch (inKind) {
+        case MALE:
+            return firstNamesMale.get(MiscUtils.randomInt(0, fnMaleMaxForRandom));
+
+        case FEMALE:
+            return firstNamesFemale.get(MiscUtils.randomInt(0,
+                    fnFemaleMaxForRandom));
+
+        default:
+            if (MiscUtils.randomInt(0, 1) == 0) {
+                return getFirstName(GENDER.MALE);
+            } else {
+                return getFirstName(GENDER.FEMALE);
+            }
+        }
+    }
+
+    public String getLastName() {
+        return lastNames.get(MiscUtils.randomInt(0, lnMaxForRandom));
+    }
+}
--- nuxeo-datademo-plugin/src/test/java/org/nuxeo/datademo/test/DataDemoTest.java
+++ nuxeo-datademo-plugin/src/test/java/org/nuxeo/datademo/test/DataDemoTest.java
@@ -17,24 +17,14 @@
 
 package org.nuxeo.datademo.test;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.nuxeo.ecm.automation.AutomationService;
-import org.nuxeo.ecm.automation.OperationChain;
-import org.nuxeo.ecm.automation.OperationContext;
-import org.nuxeo.ecm.automation.core.util.Properties;
+import org.nuxeo.datademo.RandomFirstLastNames;
+import org.nuxeo.datademo.RandomFirstLastNames.GENDER;
 import org.nuxeo.ecm.automation.test.EmbeddedAutomationServerFeature;
-import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.runtime.test.runner.Deploy;
@@ -56,7 +46,75 @@ public class DataDemoTest {
 
 
     @Test
-    public void Hop() throws Exception {
+    public void testFirstLastName() throws Exception {
+
+        RandomFirstLastNames r;
+
+        r = RandomFirstLastNames.getInstance();
+
+        String value = r.getFirstName(GENDER.MALE);
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());
+
+        value = r.getFirstName(GENDER.FEMALE);
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());
+
+        value = r.getFirstName(GENDER.ANY);
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());
+
+        value = r.getLastName();
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());
+
+        RandomFirstLastNames.release();
+
+    }
+
+    @Test
+    public void testFirstLastNameSingleton() throws Exception {
+
+        RandomFirstLastNames r1, r2, r3;
+
+        // should be in different threads, but whatever.
+        // Get 3 instances
+        r1 = RandomFirstLastNames.getInstance();
+        r2 = RandomFirstLastNames.getInstance();
+        r3 = RandomFirstLastNames.getInstance();
+        assertEquals(3, RandomFirstLastNames.getUsageCount());
+
+        // Get a value
+        String value = r1.getFirstName(GENDER.MALE);
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());
+
+        // Say thread using r1 calls release() => the singleton is not really released
+        RandomFirstLastNames.release();
+        assertEquals(2, RandomFirstLastNames.getUsageCount());
+        // "Thread" 2 can get a value
+        value = r2.getLastName();
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());
+
+        // "Thread" 2 calls release()
+        RandomFirstLastNames.release();
+        assertEquals(1, RandomFirstLastNames.getUsageCount());
+        // "Thread" 3 can get a value
+        value = r3.getFirstName(GENDER.FEMALE);
+        assertNotNull(value);
+        assertTrue(!value.isEmpty());RandomFirstLastNames.release();
+
+        // "Thread" 3 calls release()
+        RandomFirstLastNames.release();
+        assertEquals(0, RandomFirstLastNames.getUsageCount());
+
+        // Now, we must have an error
+        try {
+            value = r3.getLastName();
+        } catch (Exception e) {
+            assertEquals("NullPointerException", e.getClass().getSimpleName());
+        }
 
     }
 }
