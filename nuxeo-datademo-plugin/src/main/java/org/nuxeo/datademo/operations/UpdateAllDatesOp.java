93dc50e7cb4ac2c105bf3bd26dfb05be313e2d1d
Thibaud Arguillere <targuillere@nuxeo.com>
2015-08-16 22:09:05 -0400

    Update operations

--- nuxeo-datademo-plugin/src/main/java/org/nuxeo/datademo/operations/UpdateAllDatesOp.java
+++ nuxeo-datademo-plugin/src/main/java/org/nuxeo/datademo/operations/UpdateAllDatesOp.java
@@ -58,7 +58,7 @@ public class UpdateAllDatesOp {
             String[] names = listenersToDisable.trim().split(",");
             listenersNames = new ArrayList<String>();
             for(String oneName : names) {
-                listenersNames.add(oneName);
+                listenersNames.add(oneName.trim());
             }
         }
         
